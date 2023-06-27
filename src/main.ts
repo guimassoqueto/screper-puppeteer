76fbbeaded5cffff5195e654b859d79f9400d5f4
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-27 11:32:24 -0300

    chore: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AmazonProductPage } from './helper/amazon-product-page'
 // B09L3TWVLB - comum - f
 // B0B57HLN9K - relampago / percentual vendido - f
 // B09S6P7ZV7 - recorrencia - f
-// B09ZHZ9DN3 - meias / tenis / roupas de baixo - f
 // B08YR623QQ - ebooks - f
+// 6555156961 - livro capa comum - f
+// B0B64DBVBF - produtos com variacoes pre-selecionadas -f
+// B09ZHZ9DN3 - meias / tenis / roupas de baixo - f
 // B07FB548ZM - variacoes
 // B0BLCVLX88 - roupas
 
-const screenshot = new AmazonProductPage('B09L3TWVLB')
+const screenshot = new AmazonProductPage('<PRODUCT_ID_AMAZON>')
 
 screenshot.takeScreenshot()
   .catch(error => { console.error(error) })
