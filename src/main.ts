c1c7079b71213b006ef3634d8ee1fff851106077
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-11 09:33:18 -0300

    feat: basic implementation with redis

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { AmazonScreenshot } from "./amazon/amazon-screenshot.js";
+import { createClient } from 'redis';
 
-// TODO: implment redis to receive pids from python scrapy
-const pids =[];
+const client = createClient();
+await client.connect();
 
-for (const pid of pids) {
-  const screenshot = new AmazonScreenshot(pid);
-  await screenshot.takeScreenshot();
+let loop = true
+
+while(loop) {
+  if (client.isReady) {
+    const raw_pids = await client.get('amazon_pids');
+    if (raw_pids) { 
+      console.log("new pids found!")
+      loop = false
+      const pids: string[] = JSON.parse(raw_pids)
+      for (const pid of pids) {
+        const screenshot = new AmazonScreenshot(pid);
+        await screenshot.takeScreenshot();
+      }
+      await client.del('amazon_pids')
+      console.log("screenshots taken!")
+      loop = true
+    }
+  } else {
+    await client.connect()
+  }
 }
