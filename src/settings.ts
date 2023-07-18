a8f21f0491f3bc76db360b81ab08049a63d24354
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-18 13:03:55 -0300

    feat: update: env variables

--- src/settings.ts
+++ src/settings.ts
@@ -7,4 +7,3 @@ export const RABBITMQ_DEFAULT_PASS = process.env.RABBITMQ_DEFAULT_PASS ?? "passw
 export const RABBITMQ_DEFAULT_HOST = process.env.RABBITMQ_DEFAULT_HOST ?? "localhost"
 export const RABBITMQ_MAIN_QUEUE = process.env.RABBITMQ_MAIN_QUEUE ?? "soup-puppet"
 export const RABBITMQ_URL = `amqp://${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}@${RABBITMQ_DEFAULT_HOST}:5672`
-export const OPTIONS_ASSERT_QUEUE: Options.AssertQueue = { durable: false }
